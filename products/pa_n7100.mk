85f0595bf8c4f1ea914cf8da982e860b4d7d5684
tilaksidduram <tilaksidduram@gmail.com>
2015-04-23 10:35:48 +0530

    n7100: Update device fingerprint

--- products/pa_n7100.mk
+++ products/pa_n7100.mk
@@ -32,10 +32,17 @@ $(call inherit-product, device/samsung/n7100/full_n7100.mk)
 
 # Override AOSP build properties
 PRODUCT_NAME := pa_n7100
+PRODUCT_DEVICE := n7100
 PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := Galaxy Note II
+PRODUCT_MODEL := GT-N7100
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t03gxx TARGET_DEVICE=t03g BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.1.1/JRO03C/N7100XXALJ3:user/release-keys" PRIVATE_BUILD_DESC="t03gxx-user 4.1.1 JRO03C N7100XXALJ3 release-keys"
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=t03gxx \
+    TARGET_DEVICE=t03g \
+    PRIVATE_BUILD_DESC="t03gxx-user 4.4.2 KOT49H N7100XXUFNL1 release-keys" \
+    BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.4.2/KOT49H/N7100XXUFNL1:user/release-keys"
 
 endif
 
