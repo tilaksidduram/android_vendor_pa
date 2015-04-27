fede557df6c2217b70b085f670280a6c580b81ef
Adnan Begovic <adnan@cyngn.com>
2015-04-27 16:29:56 -0700

    Build CM Platform Library
    Change-Id: If62e6b1d2ac41730ff2a8d562173abd2cb768f93
    
    Add cmstatusbar service to system server services context
    
    Change-Id: I77c5de75722cc5f36a5326e3da57ab661b89d189
    
    Build Platform resource package.
    
    Change-Id: Id60f66b6db23989db1472a19bcb079b0083f7393
    
    vendor/cm: Lock cm platform library/cmsdk to non-release builds.
    
    Change-Id: I01c1c3fe559d438e28339ce426d7ba7e42724002
    
    Conflicts:
    	config/common.mk

--- configs/legacy_extras.mk
+++ configs/legacy_extras.mk
@@ -44,6 +44,15 @@ PRODUCT_PACKAGES += \
     org.cyanogenmod.hardware \
     org.cyanogenmod.hardware.xml
 
+# CM Platform Library Resource Package
+PRODUCT_PACKAGES += \
+    org.cyanogenmod.platform-res
+
+# CM Platform Library
+PRODUCT_PACKAGES += \
+    org.cyanogenmod.platform \
+    org.cyanogenmod.platform.xml
+
 # Extra packages
 PRODUCT_PACKAGES += \
     BluetoothExt \
