d2507b583821a755f4e8c5a6e09b9f1ecea22611
jumoog <github@jumoog.io>
2015-05-21 18:07:30 +0000

    Add klte
    Signed-off-by: jumoog <github@jumoog.io>
    
    Conflicts:
    	configs/bootanimation.mk
    	products/AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -111,6 +111,9 @@ endif
 ifeq (pa_i9300,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_i9300.mk
 endif
+ifeq (pa_klte,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_klte.mk
+endif
 ifeq (pa_i9500,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_i9500.mk
 endif
