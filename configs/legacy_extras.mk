3e0f410d7df97f8a31e52e1950981bd6933a28f7
Arne Coucheron <arco68@gmail.com>
2015-07-20 09:56:16 +0000

    Remove obsolete persist.sys.dalvik.multithread property
    Change-Id: I0d04f50dcca1454a7f38d8076b8418f67b140906

--- configs/legacy_extras.mk
+++ configs/legacy_extras.mk
@@ -23,10 +23,6 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
-# Disable multithreaded dexopt by default
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.dalvik.multithread=false
-
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
